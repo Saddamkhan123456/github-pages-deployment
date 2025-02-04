556f8491577351d8b2bb55f9647cfd0653cefa5e
Saddamkhan123456 <khansaddam444@gmail.com>
2025-02-04 16:53:24 +0530

    Updated main text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1>Hello, Welcome to application</h1>
+      <h1>Hello, Welcome to application - github actions</h1>
     </div>
   );
 }
